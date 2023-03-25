528beba2ddb9a7d318775ed4cba4c6817a64d8bd
Vladislav <zomba4o4ek100@gmail.com>
2023-03-25 13:07:21 +0300

    First task complete(2)

--- Task#1.py
+++ Task#1.py
@@ -1,28 +1,27 @@
 #Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N]. Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
-n = int(input('Enter numbers 1 to 5: '))
-c = int(input('Enter numbers 1 to 5: '))
-number = 0
-n += 1
-list = []
-for i in range(1, n):
-    list.append(i)
-    if i == c:
-        number += 1
-print(list)
-print(number)
-
-# Пытался написать через генератор рандомных чисел, но не понимаю как продолжить считать после первого прохода цикла... более того, почему увеличивается список. Надо разобраться.
 # n = int(input('Enter numbers 1 to 5: '))
-# c = int(input('Enter numbers 1 to 2: '))
-# import random
+# c = int(input('Enter numbers 1 to 5: '))
 # number = 0
 # n += 1
 # list = []
 # for i in range(1, n):
-#     list.append(random.randint(1, 2))
 #     list.append(i)
 #     if i == c:
 #         number += 1
 # print(list)
 # print(number)
 
+# Пытался написать через генератор рандомных чисел, но не понимаю как продолжить считать после первого прохода цикла... более того, почему увеличивается список. Надо разобраться.
+import random as r
+n = int(input('Enter number: '))
+c = int(input('Enter number: '))
+counter = 0
+n += 1
+list = []
+for i in range(1, n):
+    a = r.randint(1, 5)
+    list.append(a)
+    if a == c:
+        counter += 1
+print(list)
+print(counter)
